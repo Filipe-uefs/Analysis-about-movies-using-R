4ebfa61636b81be3c2cb8a2de5380d9ec6203ae7
Filipe-uefs <lipe_0109@hotmail.com>
2025-04-27 18:55:38 +0000

    Criando gráficos para variaveis

--- sintaxe.R
+++ sintaxe.R
@@ -8,13 +8,14 @@ library(jsonlite)
 #Carrega dataset
 movies_dataset = read.csv("tmdb_5000_movies.csv")
 
-set.seed(15112000) # data de nascimento como parametro para criar amostra
-movies_dataset = movies_dataset[sample(nrow(movies_dataset), 350), ]
-
 #Filtrando informações irrelevantes no dataset e tratando dados
 columns_to_ignore = c("homepage", "id", "keywords", "overview", "spoken_languages", "status", "tagline", "original_title")
 movies_dataset_filtred <- movies_dataset[, !(names(movies_dataset) %in% columns_to_ignore)]
 movies_dataset_filtred <- movies_dataset_filtred[movies_dataset_filtred$budget != 0, ]
+movies_dataset_filtred <- movies_dataset_filtred[movies_dataset_filtred$revenue != 0, ]
+
+set.seed(15112000) # data de nascimento como parametro para criar amostra
+movies_dataset_filtred = movies_dataset_filtred[sample(nrow(movies_dataset_filtred), 350), ]
 
 movies_dataset_filtred <- movies_dataset_filtred |>
   mutate(
@@ -25,6 +26,8 @@ movies_dataset_filtred <- movies_dataset_filtred |>
 
 movies_dataset_filtred <-
   movies_dataset_filtred[, !(names(movies_dataset_filtred) %in% c("genres", "production_companies", "production_countries"))]
+
+#Tabelas individuais
 
 #Tabela com os dados usando da língua usada nos filmes
 table(movies_dataset_filtred$original_language)
@@ -38,8 +41,14 @@ table(movies_dataset_filtred$production_company)
 #Tabela com os dados usando dos paises que produziram os filmes
 table(movies_dataset_filtred$production_country)
 
+
+#Tabelas agrupando duas variáveis 
+
 #Tabela com os dados relacionando línguas com genêro dos filmes
 table(movies_dataset_filtred$genre, movies_dataset_filtred$production_company)
+
+#Tabela com os dados relacionando países que produziram os filmes e produtoras
+table(movies_dataset_filtred$production_country, movies_dataset_filtred$original_language)
 
 
 # Cria gráfico comparando classificando filmes pela língua do filme
@@ -50,9 +59,31 @@ ggplot(
   labs(title = "Gráfico de quantidade de filmes produzidos por língua", x = "Língua", y = "Porcentagem") +
   scale_y_continuous(labels = scales::percent_format())
 
-#Gráfico de 
+#Gráfico de desnisdade para budget
 ggplot(movies_dataset_filtred, aes(x = budget, y =
                                   after_stat(density))) +
   geom_density(color = "green", linewidth = 1) +
   scale_x_continuous(labels = scales::label_number(big.mark = ",",
                                                    decimal.mark = "."))
+#Gráfico de desensidade de revenue
+ggplot(movies_dataset_filtred, aes(x = revenue, y =
+                                     after_stat(density))) +
+  geom_density(color = "green", linewidth = 1) +
+  scale_x_continuous(labels = scales::label_number(big.mark = ",",
+                                                   decimal.mark = "."))
+
+#Gráfico de desnsidade de média das notas do filmes
+ggplot(movies_dataset_filtred, aes(x = vote_average, y =
+                                     after_stat(density))) +
+  geom_density(color = "green", linewidth = 1) +
+  scale_x_continuous(labels = scales::label_number(big.mark = ",",
+                                                   decimal.mark = "."))
+
+media_por_genero <- movies_dataset_filtred |>
+  group_by(genre) |>
+  summarise(
+    media_vote = mean(vote_average, na.rm = TRUE),
+    n_filmes = n()
+  ) |>
+  arrange(desc(media_vote))
+
