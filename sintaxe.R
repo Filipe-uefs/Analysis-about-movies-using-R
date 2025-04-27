3a98dc93122812734b0639f028ccd660e0bd35ff
Filipe-uefs <lipe_0109@hotmail.com>
2025-04-27 13:06:03 +0000

    Criando primeiras tabelas e gráficos

--- sintaxe.R
+++ sintaxe.R
@@ -2,6 +2,8 @@
 #Carrega bibliotecas necessárias
 library(dplyr)
 library(ggplot2)
+library(jsonlite)
+
 
 #Carrega dataset
 movies_dataset = read.csv("tmdb_5000_movies.csv")
@@ -11,22 +13,38 @@ columns_to_ignore = c("homepage", "id", "keywords", "overview", "spoken_language
 movies_dataset_filtred <- movies_dataset[, !(names(movies_dataset) %in% columns_to_ignore)]
 movies_dataset_filtred <- movies_dataset_filtred[movies_dataset_filtred$budget != 0, ]
 
+movies_dataset_filtred <- movies_dataset_filtred |>
+  mutate(
+    genre = as.character(sapply(lapply(genres, fromJSON), function(x) x$name[1])),
+    production_company = as.character(sapply(lapply(production_companies, fromJSON), function(x) x$name[1])),
+    production_country = as.character(sapply(lapply(production_countries, fromJSON), function(x) x$name[1])),
+  )
+
+
+movies_dataset_filtred <-
+  movies_dataset_filtred[, !(names(movies_dataset_filtred) %in% c("genres", "production_companies", "production_countries"))]
+
+#Tabela com os dados usando da língua usada nos filmes
 table(movies_dataset_filtred$original_language)
 
-# 
-# library(tidyverse)
-# 
-# movies_dataset_filtred <- movies_dataset_filtred |>
-#   mutate(
-#     genre_names = map_chr(genres, \(g) {
-#       paste(map_chr(g, "name"), collapse = ", ")
-#     })
-#   )
+#Tabela com os dados usando do Genêro usada nos filmes
+table(unlist(movies_dataset_filtred$genre))
+
+#Tabela com os dados relacionando línguas com genêro dos filmes
+table(movies_dataset_filtred$genre, movies_dataset_filtred$original_language)
+
 
 # Cria gráfico comparando classificando filmes pela língua do filme
-ggplot(movies_dataset_filtred, aes(x = original_language, y = after_stat(prop),
-                                group = 1)) +
+ggplot(
+  movies_dataset_filtred, aes(x = original_language, y = after_stat(prop), group = 1)
+  ) +
   geom_bar(color = "blue", fill = "pink") +
   labs(title = "Gráfico de quantidade de filmes produzidos por língua", x = "Língua", y = "Porcentagem") +
   scale_y_continuous(labels = scales::percent_format())
 
+#Gráfico de 
+ggplot(movies_dataset_filtred, aes(x = budget, y =
+                                  after_stat(density))) +
+  geom_density(color = "green", linewidth = 1) +
+  scale_x_continuous(labels = scales::label_number(big.mark = ",",
+                                                   decimal.mark = "."))
